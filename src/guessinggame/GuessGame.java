7fa097c46a8da0f45e3e68d0d49de9fcdc101d81
marchill <marchill@10.0.0.3>
2019-01-27 19:49:42 -0600

    version 2

--- src/guessinggame/GuessGame.java
+++ src/guessinggame/GuessGame.java
@@ -7,19 +7,23 @@ public class GuessGame {
 
     public static void main(String[] args) {
         int n, guess;
-        
+
         Random rand = new Random();
         Scanner input = new Scanner(System.in);
-        
-        n = rand.nextInt(20);
-        System.out.print("I'm thinking of a number between 0-20.  What is the number? ");
-        guess = input.nextInt();
 
-        if (guess == n) {
-            System.out.println("You got it!");
-        } else {
-            System.out.println("Nope.  The number is " + n);
-        }
+        n = rand.nextInt(20);
+        
+        System.out.print("I'm thinking of a number between 0-20.  What is the number? ");
+
+        do {
+            guess = input.nextInt();
+
+            if (guess == n) {
+                System.out.println("You got it!");
+            } else {
+                System.out.print("Nope.  Try again: ");
+            }
+        } while (guess != n);
     }
-    
+
 }
